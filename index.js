0df904b65a512de42c13fc59e3b23e70f13d55d8
Calvin Metcalf <cmetcalf@appgeo.com>
2014-03-22 19:59:49 -0400

    more changes

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var PouchDB = require('pouchdb');
 inherits(PouchStream, Transform);
 module.exports = PouchStream;
 function PouchStream(db, opts) {
+  if (!(this instanceof PouchStream)) {
+    return new PouchStream(db, opts);
+  }
+  Transform.call(this, {
+    objectMode: true,
+    decodeStrings: false
+  });
   opts = opts || {};
   this.batchSize = opts.batchSize || 100;
   if (typeof db === 'string') {
@@ -11,4 +18,26 @@ function PouchStream(db, opts) {
   } else {
     this.db = db;
   }
+  this.queue = [];
 }
+
+PouchStream.prototype._transform = function (chunk, _, next) {
+  this.queue.push(chunk);
+  if (this.queue.length < this.batchSize) {
+    return next();
+  }
+  this._flush(next);
+};
+PouchStream.prototype._flush = function (next) {
+  var self = this;
+  var payload = {
+    docs: this.queue
+  };
+  this.queue = [];
+  this.db.bulkDocs(payload).then(function (resp) {
+    resp.forEach(function (thing) {
+      self.push(thing);
+    });
+    next();
+  }, next);
+};
--- test/test.js
+++ test/test.js
@@ -1,21 +1,102 @@
-var PouchDB = require("pouchdb");
-var should = require("chai").should;
-var PouchStream = require('../');
+var test = require('tape');
+var pStream = require('../');
+var PouchDB = require('pouchdb');
+var through = require('through2').obj;
 
-describe('PouchStream', function(){
-  var db;
-  function before(done){
-    new PouchDB('test', function(err, d){
-        db = d;
-        done(err);
+test('basic', function (t) {
+  var db = new PouchDB('testBasic');
+  t.test('test', function (t) {
+    var stream = new pStream(db);
+    stream.on('error', function (err) {
+      t.error(err);
     });
-  }
-  function after(done){
-    PouchDB.destroy('test', function(err){
-      done(err);
-    })
-  }
-  it('should work', function(done){
-    done();
+    var out = [];
+    var oStream = through(function (chunk, _, next) {
+
+      out.push(chunk);
+      next();
+    }, function (next) {
+        t.equals(2, out.length, 'should be 2 docs');
+        t.end();
+        next();
+    });
+     stream.pipe(oStream);
+    stream.write({name: 'jeff'}, function () {
+      stream.write({name: 'henry'}, function () {
+        stream.end();
+      });
+    });
+
+  });
+
+  t.test('after', function (t) {
+    db.destroy(function () {
+      t.end();
+    });
+  });
+});
+
+test('advanced', function (t) {
+  var db = new PouchDB('testAdvanced');
+  t.test('test', function (t) {
+    var stream = new pStream(db, {batchSize: 3});
+    var out = [];
+    var oStream = through(function (chunk, _, next) {
+      out.push(chunk);
+      next();
+    }, function (next) {
+        t.equals(5, out.length, 'should be 5 docs');
+        t.end();
+        next();
+    });
+    stream.pipe(oStream);
+    stream.write({name: 'jeff'});
+    stream.write({name: 'henry'});
+    stream.write({name: 'james'});
+    stream.write({name: 'greg'});
+    stream.write({name: 'nick'});
+    stream.end();
+  });
+
+  t.test('after', function (t) {
+    db.destroy(function () {
+      t.end();
+    });
+  });
+});
+test('super advanced', function (t) {
+  var db = new PouchDB('testSuperAdvanced');
+  t.test('test', function (t) {
+    var stream = new pStream(db, {batchSize: 10});
+    var out = [];
+    var oStream = through(function (chunk, _, next) {
+      out.push(chunk);
+      next();
+    }, function (next) {
+        t.equals(50, out.length, 'should be 50 docs');
+        t.end();
+        next();
+    });
+    stream.pipe(oStream);
+    var i = 50;
+    function putIt () {
+      stream.write({'_id':'' + (i--)}, function (err){
+        if (err) {
+          t.error(err);
+        }
+        if (i) {
+          process.nextTick(putIt);
+        } else {
+          stream.end();
+        }
+      });
+    }
+    putIt();
+  });
+
+  t.test('after', function (t) {
+    db.destroy(function () {
+      t.end();
+    });
   });
 });
